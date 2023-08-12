a08a5dbf89b00b4ec9e04f01cfc6e43fe5aa5d6c
Egy_ember <kamufiok06@gmail.com>
2023-08-12 23:56:02 +0200

    why segfault?

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc ./test.c -lX11 -g -o ./test
+gcc ./test.c -lX11 -g -fstack-check -fbounds-check -o ./test
 chmod +x ./test
--- test.c
+++ test.c
@@ -25,13 +25,15 @@ img100* loadimg100(char* PATH){
 		char small[4];
 		char big[80];
 	}tmp;
+	//todo: fix this
+
 	fgets(tmp.small, 4, fptr);
 	if(strcmp(tmp.small,  "P6\n")){
 		printf("szar file \n");
 		printf("magic byte: %s\n", tmp.small);
 		exit(ERROR);
 	};
-	fgets(tmp.big, 80, fptr); //todo: fix this
+	fgets(tmp.big, 80, fptr);
 	if(tmp.big[0] == '#'){
 		printf("skiping comment\n");
 		fgets(tmp.big, 80, fptr);
@@ -47,11 +49,45 @@ img100* loadimg100(char* PATH){
 		printf("img maxrgb: %s\n", tmp.big);
 		exit(ERROR);
 	};
-	printf("file good");
+	printf("file good\n");
 	fclose(fptr);
-	//fopen(PATH, fptr, "rb");
-	img100* outprt=(img100*)  malloc( sizeof(img100));
-	
+	fptr = fopen(PATH, "rb");
+	img100* outprt=(img100*)  malloc( sizeof(img100)); //alloc memory for the rgb map
+	printf("allocated %lu mem at: %p",sizeof(img100), outprt);
+	int readbuffer[300]; //read buffer
+	fseek(fptr, 61, SEEK_SET); //todo: make this not only accept files from gimp
+			//for(int x =0; ;x++){
+				size_t readbytes = fread(
+					readbuffer, 
+					sizeof(int),
+					300,
+					fptr
+					);
+			/*	int y =0;
+				printf("readbytes: %zu\n", readbytes);
+				for(int bufferID=0; bufferID<readbytes;bufferID+=3){
+					printf("asdasd bufferID=%d x=%d y=%d\n", bufferID, x, y);
+					*outprt[x][y][0] = readbuffer[bufferID];
+					//printf("*outprt [%d][%d][0] = %hhu\n", x, y, readbuffer[bufferID]);
+					*outprt[x][y][1] = readbuffer[bufferID+1];
+					//printf("*outprt [%d][%d][1] = %hhu\n", x, y, readbuffer[bufferID+1]);
+					*outprt[x][y][2] = readbuffer[bufferID+2];
+					//printf("*outprt [%d][%d][2] = %hhu\n", x, y, readbuffer[bufferID+2]);
+					y++;
+					printf("y = %d, bufferID = %d\n", y, bufferID);
+				};
+				if(readbytes ==0){
+					printf("this thing happen\n");
+					break;
+				};
+			*/
+//this shoud segfault
+int x = 2;
+int y = 10;
+int bufferID=30;
+printf("%d\n",*outprt[x][y][0]);
+*outprt[x][y][0] = readbuffer[bufferID];
+			//}
 	fclose(fptr);
 	return outprt;
 };
@@ -133,7 +169,7 @@ int main(){
 	for(int i;numberOfFonts>i;i++){
 		printf("font[%d]%s \n", i,fontlist[i]);
 	};
-	sleep(1);
+	//sleep(1);
 
 	//load font
 	Font fontId = XLoadFont(d, fontlist[0]);
@@ -151,7 +187,7 @@ int main(){
 	*/
 
 	//stirng width
-	char testString[] = "árvíztürő tűkörfúrógép"; // todo: convert it to ISO 8856-2 or ISO 10xx the fuck know the number
+	char testString[] = "árvíztürő tűkörfúrógép"; // todo: convert it to ISO 8856-2 or ISO 10xxx the fuck know the number
 	printf("Teszt string: %s\n", testString);
 	int testStringWidth;
 	testStringWidth = XTextWidth(fontStruct, testString, stringLen(testString));
@@ -165,15 +201,21 @@ int main(){
 	XDrawString(d, Win, gcBlack, 30, 30, testString, stringLen(testString));
 		//flush Xbuffer
 	XFlush(d);
-	sleep(10);
+	//sleep(10);
 //next task load an image and put it to the sceen
 //todo: load ppm files to memory
 //todo: design map and entity data structures
 //todo load map data from a custom file format
 //todo: render image from loaded ppm files (with the cpu)
 
-	//loadimg100("./img/player.ppm");
-	
+	img100* playerImg =loadimg100("./img/player.ppm");
+
+	printf("[x=0][y=0][red]: %d\n", *playerImg[0][0][0]);
+	printf("[x=0][y=0][green]: %d\n", *playerImg[0][0][1]);
+	printf("[x=0][y=0][blue]: %d\n", *playerImg[0][0][2]);
+	printf("[x=0][y=100][red]: %d\n", *playerImg[0][99][0]);
+	printf("[x=1][y=0][red]: %d\n", *playerImg[1][0][0]);
+
 	//unload font
 	XUnloadFont(d, fontId);
 	//close connection&destroy window
